e4d7bdf0a60e18d870620661ab132357196cd368
Gaurav Mittal <gamittal@akamai.com>
2024-09-17 21:44:11 -0700

    removed extra spaces

--- src/credentials.py
+++ src/credentials.py
@@ -1,7 +1,8 @@
+from pathlib import Path
+from urllib.parse import urljoin
+
 from akamai.edgegrid import EdgeRc, EdgeGridAuth
 from requests import Session
-from urllib.parse import urljoin
-from pathlib import Path
 
 EDGERC = EdgeRc(str(Path.home().joinpath('.edgerc')))
 SECTION = 'default'
@@ -9,13 +10,16 @@ baseurl = f'https://{EDGERC.get(SECTION, "host")}'
 session = Session()
 session.auth = EdgeGridAuth.from_edgerc(EDGERC, SECTION)
 
+
 def _get_all_switch_keys(my_account):
     qparam = {
         'search': my_account
     }
 
-    resp = session.get(urljoin(baseurl, 'identity-management/v3/api-clients/self/account-switch-keys'), params=qparam).json()
+    resp = session.get(urljoin(baseurl, 'identity-management/v3/api-clients/self/account-switch-keys'),
+                       params=qparam).json()
     return resp
+
 
 def generate_switch_key():
     my_account = input('Enter account name: ')
@@ -23,8 +27,9 @@ def generate_switch_key():
     for i, account in enumerate(accounts, start=1):
         print(f'{i}: {account["accountName"]}')
     key_number = int(input('Enter key number: '))
-    switch_key =accounts[key_number -1]['accountSwitchKey']
+    switch_key = accounts[key_number - 1]['accountSwitchKey']
     return switch_key
+
 
 if __name__ == '__main__':
     print(generate_switch_key())
